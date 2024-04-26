5d52c764f7312666fcec6217f1c264d30d3dade2
HanadiJad <168185240+HanadiJad@users.noreply.github.com>
2024-04-26 20:52:24 +0300

    Update IngredientColor.java

--- src/main/java/com/mycompany/cocktail/IngredientColor.java
+++ src/main/java/com/mycompany/cocktail/IngredientColor.java
@@ -1,14 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package com.mycompany.cocktail;
-
-/**
- *
- * @author user
- */
-
 
 public interface IngredientColor {
     Colors getColor();
